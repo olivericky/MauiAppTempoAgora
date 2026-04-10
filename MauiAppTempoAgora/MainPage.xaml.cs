7e7c8d153c929a52d7f70cf5ed2dd75ceb6474a2
olivericky <ricardo_luiz16@hotmail.com>
2026-04-09 23:12:56 -0300

    Implementação da Aula 08 de DS III.

--- MauiAppTempoAgora/MainPage.xaml.cs
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -5,14 +5,13 @@ namespace MauiAppTempoAgora
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private async void Button_Clicked(object sender, EventArgs e)
+        private async void Button_Clicked_Previsao(object sender, EventArgs e)
         {
             try
             {
@@ -33,6 +32,13 @@ namespace MauiAppTempoAgora
 
                         lbl_res.Text = dados_previsao;
 
+                        string mapa = $"https://embed.windy.com/embed.html?" +
+                                      $"type=map&location=coordinates&metricRain=mm&metricTemp=°C" +
+                                      $"&metricWind=km/h&zoom=5&overlay=wind&product=ecmwf&level=surface" +
+                                      $"&lat={t.lat.ToString().Replace(",",".")}&lon={t.lon.ToString().Replace(",", ".")}";
+
+                        mv_mapa.Source = mapa;
+
                     }
                     else
                     {
@@ -52,6 +58,66 @@ namespace MauiAppTempoAgora
                 await DisplayAlert("Ops", ex.Message, "OK");
             }
         }
-    }
 
+        private async void Button_Clicked_Localizacao(object sender, EventArgs e)
+        {
+            try
+            {
+                GeolocationRequest request = new GeolocationRequest(
+                    GeolocationAccuracy.Medium,
+                    TimeSpan.FromSeconds(10)
+                );
+
+                Location? local = await Geolocation.Default.GetLocationAsync(request);
+
+                if (local != null)
+                {
+                    string local_disp = $"Latitude: {local.Latitude} \n" +
+                                        $"Longitude: {local.Longitude}";
+
+                    lbl_coords.Text = local_disp;
+
+                    // pega nome da cidade que está nas coordenadas.
+                    GetCidade(local.Latitude, local.Longitude);
+
+                }
+                else 
+                { 
+                    lbl_coords.Text = "Nenhuma localização";
+                }
+
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                await DisplayAlert("Erro: Dispositivo não Suporta", fnsEx.Message, "OK");
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                await DisplayAlert("Erro: Localização Desabilitada", fneEx.Message, "OK");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro", ex.Message, "OK");
+            }
+        }
+
+        private async void GetCidade(double lat, double lon)
+        {
+            try
+            {
+                IEnumerable<Placemark> places = await Geocoding.Default.GetPlacemarksAsync(lat, lon);
+
+                Placemark? place = places.FirstOrDefault();
+
+                if (place != null)
+                {
+                    txt_cidade.Text = place.Locality;
+                }
+            } catch(Exception ex)
+            {
+                await DisplayAlert("Erro: Obtenção do nome da cidade", ex.Message, "OK");
+            }
+
+        }
+    }
 }
